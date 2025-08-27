93f4b834528c6dd9b52b947b600a0f14c6bf0787
abhijithmahipal <abhijithmahipal@gmail.com>
2025-08-27 18:42:15 +0530

    Remove unused output configuration and exportPathMap from Next.js config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true
   },
-  trailingSlash: true,
-  exportPathMap: async function() {
-    return {
-      '/': { page: '/' },
-      '/houses': { page: '/houses' }
-    }
-  }
+  trailingSlash: true
 }
 
 module.exports = nextConfig
